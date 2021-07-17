f87620cf5ef32f2cc1be84bf949c12b1b52dc92f
sachinshukla636 <sachinshukla636@gmail.com>
2021-07-17 22:31:00 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ export default class App extends React.Component {
   }
   componentDidUpdate(prevProps, preState, snapshot) {
     console.log('componentDidUpdate', preState);
+    if (this.state.counter + 1 < 10) {
+      this.setState({ counter: this.state.counter + 1 });
+    }
   }
   render() {
     console.log('render');
